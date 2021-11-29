5e23cf5aac613231171e37ee53344ea5d38e284f
Tandeep Gill <gill.tandeep@gmail.com>
2021-11-29 21:42:58 +0000

    MainItinerary, initial styling

--- client/components/Day/Day.js
+++ client/components/Day/Day.js
@@ -17,47 +17,69 @@ class Day extends React.Component {
           <tbody>
             <tr className='border-b border-black'>
               <td className='border-r border-black'>7:00 - 8:00</td>
-              <td></td>
+              <td>
+                <ul></ul>
+              </td>
             </tr>
             <tr className='border-b border-black'>
               <td className='border-r border-black'>8:00 - 9:00</td>
-              <td></td>
+              <td>
+                <ul></ul>
+              </td>
             </tr>
             <tr className='border-b border-black'>
               <td className='border-r border-black'>9:00 - 10:00</td>
-              <td></td>
+              <td>
+                <ul></ul>
+              </td>
             </tr>
             <tr className='border-b border-black'>
               <td className='border-r border-black'>11:00 - 12:00</td>
-              <td></td>
+              <td>
+                <ul></ul>
+              </td>
             </tr>
             <tr className='border-b border-black'>
               <td className='border-r border-black'>1:00 - 2:00</td>
-              <td></td>
+              <td>
+                <ul></ul>
+              </td>
             </tr>
             <tr className='border-b border-black'>
               <td className='border-r border-black'>2:00 - 3:00</td>
-              <td></td>
+              <td>
+                <ul></ul>
+              </td>
             </tr>
             <tr className='border-b border-black'>
               <td className='border-r border-black'>3:00 - 4:00</td>
-              <td></td>
+              <td>
+                <ul></ul>
+              </td>
             </tr>
             <tr className='border-b border-black'>
               <td className='border-r border-black'>5:00 - 6:00</td>
-              <td></td>
+              <td>
+                <ul></ul>
+              </td>
             </tr>
             <tr className='border-b border-black'>
               <td className='border-r border-black'>6:00 - 7:00</td>
-              <td></td>
+              <td>
+                <ul></ul>
+              </td>
             </tr>
             <tr className='border-b border-black'>
               <td className='border-r border-black'>7:00 - 8:00</td>
-              <td></td>
+              <td>
+                <ul></ul>
+              </td>
             </tr>
             <tr className='border-b border-black'>
               <td className='border-r border-black'>8:00 - 9:00</td>
-              <td></td>
+              <td>
+                <ul></ul>
+              </td>
             </tr>
           </tbody>
         </table>
