0e2e9d54d48915cd38b4eb27ac26b97e5d0273d9
Jeffrey Wood <jwfirenze@gmail.com>
2021-11-30 13:51:12 -0500

    drag and drop works

--- client/components/Itinerary/Itinerary.js
+++ client/components/Itinerary/Itinerary.js
@@ -11,6 +11,53 @@ import { useState } from "react";
 
 const trips = [
   {
+    id: 1,
+    name: "Visit Statue of Liberty",
+    location: "Liberty Island, New York, NY",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    imageUrl:
+      "https://a.cdn-hotels.com/gdcs/production6/d1738/3c1a71e7-0a73-4810-9935-5c4daea1954e.jpg?impolicy=fcrop&w=800&h=533&q=medium",
+    attendees:
+      "https://t3.ftcdn.net/jpg/02/22/85/16/360_F_222851624_jfoMGbJxwRi5AWGdPgXKSABMnzCQo9RN.jpg",
+  },
+  {
+    id: 2,
+    name: "Visit the Met Museum",
+    location: "5 Museum Mile, New York, NY",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    imageUrl:
+      "http://www.metmuseum.org/-/media/images/visit/met-fifth-avenue/fifthave_teaser.jpg?sc_lang=en",
+    attendees:
+      "https://t3.ftcdn.net/jpg/02/22/85/16/360_F_222851624_jfoMGbJxwRi5AWGdPgXKSABMnzCQo9RN.jpg",
+  },
+  {
+    id: 3,
+    name: "Visit Freedom Tower",
+    location: "Liberty Island, New York, NY",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    imageUrl:
+      "https://www.nydailynews.com/resizer/EsyO7of502AOt3lM9wrqf4NCYOk=/1200x0/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/SUNBWE47ACEVH67NCDESD2RHJ4.jpg",
+    attendees:
+      "https://t3.ftcdn.net/jpg/02/22/85/16/360_F_222851624_jfoMGbJxwRi5AWGdPgXKSABMnzCQo9RN.jpg",
+  },
+  {
+    id: 4,
+    name: "Have Dinner at Lucalis",
+    location: "5 Evans Street, New York, NY",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    imageUrl:
+      "https://infatuation.imgix.net/media/images/reviews/lucali/TeddyWolff.Lucali.Interiors.16.jpg?auto=format&w=256",
+    attendees:
+      "https://t3.ftcdn.net/jpg/02/22/85/16/360_F_222851624_jfoMGbJxwRi5AWGdPgXKSABMnzCQo9RN.jpg",
+  },
+];
+
+const unassignedTrips = [
+  {
     id: 221,
     name: "Visit Statue of Liberty",
     location: "Liberty Island, New York, NY",
@@ -58,27 +105,52 @@ const trips = [
 
 const Itinerary = () => {
   let [tripList, updateTripList] = useState(trips);
+  let [unassigned, updateUnassigned] = useState(unassignedTrips);
+
+  const tripObj = {
+    day1: tripList,
+    unassigned: unassigned,
+  };
+  const tripObjMethods = {
+    day1: updateTripList,
+    unassigned: updateUnassigned,
+  };
   function handleOnDragEnd(result) {
+    console.log(result);
     if (!result.destination) return;
+
     const items = trips;
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-    updateTripList(items);
+    const [reorderedItem] = tripObj[result.source.droppableId].splice(
+      result.source.index,
+      1
+    );
+    tripObj[result.destination.droppableId].splice(
+      result.destination.index,
+      0,
+      reorderedItem
+    );
+    // updateTripList(items);
+    tripObjMethods[result.source.droppableId](
+      tripObj[result.source.droppableId]
+    );
+    tripObjMethods[result.destination.droppableId](
+      tripObj[result.destination.droppableId]
+    );
   }
 
   return (
     <>
-      <div className="grid m-3 gap-6 grid-cols-12">
-        <div className="flex flex-col col-span-6 ">
-          <div className="my-2 flex space-around">
-            <button className="p-2 border-2 block rounded-md">Day 1</button>
-            <button className="p-2 border-2 block rounded-md">Day 2</button>
-            <button className="p-2 border-2 block rounded-md">Day 3</button>
-          </div>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <div className="grid m-3 gap-6 grid-cols-12">
+          <div className="flex flex-col col-span-6 ">
+            <div className="my-2 flex space-around">
+              <button className="p-2 border-2 block rounded-md">Day 1</button>
+              <button className="p-2 border-2 block rounded-md">Day 2</button>
+              <button className="p-2 border-2 block rounded-md">Day 3</button>
+            </div>
 
-          <div className="flex">
-            <DragDropContext onDragEnd={handleOnDragEnd}>
-              <Droppable droppableId="trips">
+            <div className="flex">
+              <Droppable droppableId="day1">
                 {(provided) => (
                   <ul {...provided.droppableProps} ref={provided.innerRef}>
                     {tripList.map((trip, index) => {
@@ -112,31 +184,29 @@ const Itinerary = () => {
                   </ul>
                 )}
               </Droppable>
-            </DragDropContext>
+            </div>
+          </div>
+
+          <div className="col-span-6 p-6 flex items-center">
+            <img
+              src="https://www.tripsavvy.com/thmb/1OWt6nE-xYX5v9CkEkHSbL4cRCg=/882x766/filters:fill(auto,1)/Google-Maps-5--58e4125e5f9b58ef7e4c582d.png"
+              alt="New York"
+            />
           </div>
         </div>
 
-        <div className="col-span-6 p-6 flex items-center">
-          <img
-            src="https://www.tripsavvy.com/thmb/1OWt6nE-xYX5v9CkEkHSbL4cRCg=/882x766/filters:fill(auto,1)/Google-Maps-5--58e4125e5f9b58ef7e4c582d.png"
-            alt="New York"
-          />
-        </div>
-      </div>
-
-      <div>
-        <h2 className="font-semibold underline">Unassigned:</h2>
-        <div className="grid m-3 gap-6 grid-cols-12">
-          <div className="flex col-span-12">
-            <DragDropContext onDragEnd={handleOnDragEnd}>
-              <Droppable droppableId="trips">
+        <div>
+          <h2 className="font-semibold underline">Unassigned:</h2>
+          <div className="grid m-3 grid-cols-12">
+            <div className="flex col-span-12">
+              <Droppable droppableId="unassigned">
                 {(provided) => (
-                  <ul
+                  <div
                     {...provided.droppableProps}
                     ref={provided.innerRef}
-                    className="flex flex-row"
+                    className="flex flex-row space-even"
                   >
-                    {tripList.map((trip, index) => {
+                    {unassignedTrips.map((trip, index) => {
                       return (
                         <Draggable
                           key={trip.id}
@@ -144,10 +214,11 @@ const Itinerary = () => {
                           index={index}
                         >
                           {(provided) => (
-                            <li
+                            <div
                               {...provided.draggableProps}
                               {...provided.dragHandleProps}
                               ref={provided.innerRef}
+                              className="w-1/6 h-12"
                             >
                               <EventCard
                                 id={trip.id}
@@ -155,19 +226,19 @@ const Itinerary = () => {
                                 location={trip.location}
                                 imageUrl={trip.imageUrl}
                               />
-                            </li>
+                            </div>
                           )}
                         </Draggable>
                       );
                     })}
                     {provided.placeholder}
-                  </ul>
+                  </div>
                 )}
               </Droppable>
-            </DragDropContext>
+            </div>
           </div>
         </div>
-      </div>
+      </DragDropContext>
     </>
   );
 };
