cf794ba73cad08819e23baf0765c70548c822253
Jeffrey Wood <jwfirenze@gmail.com>
2021-11-30 16:57:50 -0500

    api itinerary userid route sends users itinerary event objects

--- server/api/itinerary.js
+++ server/api/itinerary.js
@@ -1,17 +1,21 @@
 const router = require("express").Router();
 const Sequelize = require("sequelize");
 const {
-  models: { Itinerary, User, UsersItineraries },
+  models: { Itinerary, User, ItineraryEvents },
 } = require("../db");
 
 module.exports = router;
 
+//api/itinerary/userId => gets user's itinerary
 router.get("/:userId", async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.userId);
-    const itinerary = await user.getItineraries();
-    console.log(itinerary);
-    res.send(itinerary);
+    const itinerary = await user.getItineraries({});
+    const itineraryId = itinerary[0].id;
+    const events = await ItineraryEvents.findAll({
+      where: { itineraryId: itineraryId },
+    });
+    res.send(events);
   } catch (error) {
     next(error);
   }
