8f1c752cf1f21687fd53e25d9d31ac28f4ea4690
Tandeep Gill <gill.tandeep@gmail.com>
2021-12-01 21:05:36 +0000

    CreateItinerary, Express route created for single user.

--- server/api/users.js
+++ server/api/users.js
@@ -1,17 +1,43 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
+const router = require('express').Router();
+const { User } = require('../db/index');
+module.exports = router;
 
+// GET /users
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
+      attributes: [
+        'id',
+        'username',
+        'email',
+        'firstName',
+        'lastName',
+        'avatar',
+      ],
+    });
+    res.send(users);
+  } catch (error) {
+    next(error);
   }
-})
+});
+
+// GET /users/userId
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId);
+    let safeUserData = {
+      id: user.dataValues.id,
+      username: user.dataValues.username,
+      email: user.dataValues.email,
+      firstName: user.dataValues.firstName,
+      lastName: user.dataValues.lastName,
+      avatar: user.dataValues.avatar,
+    };
+    res.send(safeUserData);
+  } catch (error) {
+    next(error);
+  }
+});
