c2a1c58723b6257c7c4b9f0191b787bf29d1b29c
Tandeep Gill <gill.tandeep@gmail.com>
2021-12-01 16:17:07 +0000

    CreateItinerary, seed file updated with data.

--- script/seed.js
+++ script/seed.js
@@ -1,29 +1,114 @@
-'use strict'
+'use strict';
 
-const {db, models: {User} } = require('../server/db')
+const {
+  db,
+  models: { User, Events, Itinerary },
+} = require('../server/db');
 
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
 async function seed() {
-  await db.sync({ force: true }) // clears db and matches models to tables
-  console.log('db synced!')
+  await db.sync({ force: true }); // clears db and matches models to tables
+  console.log('db synced!');
 
   // Creating Users
   const users = await Promise.all([
-    User.create({ username: 'cody', password: '123' }),
-    User.create({ username: 'murphy', password: '123' }),
-  ])
+    User.create({
+      username: 'cody',
+      password: '123',
+      email: 'cdog@gmail.com',
+      firstName: 'cody',
+      lastName: 'dog',
+    }),
+    User.create({
+      username: 'murphy',
+      password: '123',
+      email: 'mdog@gmail.com',
+      firstName: 'murphy',
+      lastName: 'gomez',
+    }),
+  ]);
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
-  return {
-    users: {
-      cody: users[0],
-      murphy: users[1]
-    }
-  }
+  //Create EVents
+  const events = await Promise.all([
+    Events.create({
+      eventType: 'Attraction',
+      name: 'Visit Statue of Liberty',
+      latitude: '70',
+      longitude: '-40',
+      location: 'Liberty Island, New York, NY',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      imageUrl:
+        'https://a.cdn-hotels.com/gdcs/production6/d1738/3c1a71e7-0a73-4810-9935-5c4daea1954e.jpg?impolicy=fcrop&w=800&h=533&q=medium',
+    }),
+    Events.create({
+      eventType: 'Attraction',
+      name: 'Visit the Met Museum',
+      latitude: '70',
+      longitude: '-40',
+      location: '5 Museum Mile, New York, NY',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      imageUrl:
+        'http://www.metmuseum.org/-/media/images/visit/met-fifth-avenue/fifthave_teaser.jpg?sc_lang=en',
+    }),
+    Events.create({
+      eventType: 'Attraction',
+      name: 'Visit Freedom Tower',
+      latitude: '70',
+      longitude: '-40',
+      location: 'Liberty Island, New York, NY',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      imageUrl:
+        'https://www.nydailynews.com/resizer/EsyO7of502AOt3lM9wrqf4NCYOk=/1200x0/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/SUNBWE47ACEVH67NCDESD2RHJ4.jpg',
+    }),
+    Events.create({
+      eventType: 'Attraction',
+      name: 'Have Dinner at Lucalis',
+      latitude: '70',
+      longitude: '-40',
+      location: '5 Evans Street, New York, NY',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      imageUrl:
+        'https://infatuation.imgix.net/media/images/reviews/lucali/TeddyWolff.Lucali.Interiors.16.jpg?auto=format&w=256',
+    }),
+    Events.create({
+      eventType: 'Attraction',
+      name: 'Walk in Central Park',
+      latitude: '70',
+      longitude: '-40',
+      location: 'Central Park',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      imageUrl:
+        'https://assets.centralparknyc.org/media/images/_1650x767_crop_center-center_none/Bethesda-Terrace_20190515_002.jpg',
+    }),
+    Events.create({
+      eventType: 'Attraction',
+      name: 'See Phantom of The Opera',
+      latitude: '70',
+      longitude: '-40',
+      location: 'Majestic Theatre',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      imageUrl:
+        'https://static.wikia.nocookie.net/sexypedia/images/f/f8/Phantom.jpg',
+    }),
+  ]);
+
+  //Create itinerary
+  const itinerary = await Promise.all([
+    Itinerary.create({
+      name: 'Codys New York Trip',
+      startDate: '2021-11-30',
+      endDate: '2021-12-20',
+    }),
+  ]);
 }
 
 /*
@@ -32,16 +117,16 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log('seeding...')
+  console.log('seeding...');
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log('closing db connection');
+    await db.close();
+    console.log('db connection closed');
   }
 }
 
@@ -51,8 +136,8 @@ async function runSeed() {
   any errors that might occur inside of `seed`.
 */
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
