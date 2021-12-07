ec36f38b7a03f30cc54d5f0c9275f7a173d5c74c
Tandeep Gill <gill.tandeep@gmail.com>
2021-12-07 22:51:49 +0000

    LiveChat somewhat styled.

--- client/components/ChatHome/ChatHome.js
+++ client/components/ChatHome/ChatHome.js
@@ -1,38 +1,42 @@
-import React, { useState } from "react";
-import { useSelector } from 'react-redux'
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const ChatHome = () => {
-  const [roomName, setRoomName] = useState("");
+  const [roomName, setRoomName] = useState('');
 
-  const rooms = useSelector(state => state.itineraries)
+  const rooms = useSelector((state) => state.itineraries);
 
-  let roomList = rooms.length > 0 && rooms.map((room, i) => {
-    return (
-      <option
-        key={room.id}
-        value={room.id}
-      >
-        {room.name}
-      </option>
-    )
-  })
+  let roomList =
+    rooms.length > 0 &&
+    rooms.map((room, i) => {
+      return (
+        <option key={room.id} value={room.id}>
+          {room.name}
+        </option>
+      );
+    });
 
   return (
-    <div className="home-container">
+    <div className='w-1/6 flex flex-col'>
       <select
-        name="rooms"
+        name='rooms'
         value={roomName}
         onChange={(e) => setRoomName(e.target.value)}
       >
-        <option hidden value="">Room List</option>
+        <option hidden value=''>
+          Room List
+        </option>
         {roomList}
       </select>
-      <Link to={`/livechat/${roomName}`} className="enter-room-button">
+      <Link
+        to={`/livechat/${roomName}`}
+        className='mt-5 text-xl bg-green-400 text-white font-semibold text-center no-underline rounded-md'
+      >
         Join room
       </Link>
     </div>
   );
 };
 
-export default ChatHome
+export default ChatHome;
--- client/components/LiveChat/LiveChat.js
+++ client/components/LiveChat/LiveChat.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-import useChat from "./useChat";
+import useChat from './useChat';
 
 const LiveChat = (props) => {
   const { roomId } = props.match.params; // Gets roomId from URL
   const { messages, sendMessage } = useChat(roomId); // Creates a websocket and manages messaging
-  const [newMessage, setNewMessage] = React.useState(""); // Message to be sent
-  const userFirstName = useSelector(state => state.auth.firstName)
+  const [newMessage, setNewMessage] = React.useState(''); // Message to be sent
+  const userFirstName = useSelector((state) => state.auth.firstName);
 
   const handleNewMessageChange = (event) => {
     setNewMessage(event.target.value);
@@ -15,36 +15,45 @@ const LiveChat = (props) => {
 
   const handleSendMessage = () => {
     sendMessage(newMessage, userFirstName);
-    setNewMessage("");
+    setNewMessage('');
   };
-
+  // border border-gray-200 border-solid rounded h-1/4
   return (
-    <div className="chat-room-container">
-      <h1 className="room-name">Room: {roomId}</h1>
-      <div className="messages-container">
-        <ol className="messages-list">
+    <div className='flex flex-col m-4 w-1/6'>
+      <h1 className='mt-0'>Room: {roomId}</h1>
+      <div className='h-52 border border-gray-400 border-solid rounded overflow-auto mb-1'>
+        <ol className='list-none p-0'>
           {messages.map((message, i) => (
-            <>
-            {message.userFirstName}
-            <li
-              key={i}
-              className={`message-item ${
-                message.ownedByCurrentUser ? "my-message" : "received-message"
+            <div
+              className={`flex flex-col m-1.5 ${
+                message.ownedByCurrentUser ? 'items-end' : 'items-start'
               }`}
             >
-              {message.body}
-            </li>
-            </>
+              <h6 className='text-xs text-gray-400'>{message.userFirstName}</h6>
+              <li
+                key={i}
+                className={`rounded text-white ${
+                  message.ownedByCurrentUser
+                    ? 'bg-blue-600 ml-auto text-sm px-1 py-0.5'
+                    : 'bg-gray-800 mr-auto text-sm px-1 py-0.5'
+                }`}
+              >
+                {message.body}
+              </li>
+            </div>
           ))}
         </ol>
       </div>
       <textarea
         value={newMessage}
         onChange={handleNewMessageChange}
-        placeholder="Write message..."
-        className="new-message-input-field"
+        placeholder='Write message...'
+        className='h-18 text-sm resize-none bg-gray-100 p-1 border rounded'
       />
-      <button onClick={handleSendMessage} className="send-message-button">
+      <button
+        onClick={handleSendMessage}
+        className='mt-3 text-base bg-green-400 text-white font-semibold text-center no-underline rounded-md'
+      >
         Send
       </button>
     </div>
